057310a9e1b573524e9033862d500f2fe8f69d8f
PC-20180317KOZR\Administrator <dvdjvdvdvdd>
2018-10-22 11:06:19 +0800

    我的个人中心,跳转的页面,

--- pages/GasInformation/GasInformation.js
+++ pages/GasInformation/GasInformation.js
@@ -162,7 +162,7 @@ Page({
     let telphone = /^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/;
     if (!telphone.test(phone)) {
       this.setData({
-        storename: ""
+        telephone: ""
       })
       wx.showToast({
         title: "电话输入有误！",
