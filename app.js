c847ab686c434864701e1d12690d139c90ef0512
HBY <450932358@.com>
2018-10-25 14:03:11 +0800

    no message

--- app.js
+++ app.js
@@ -2,17 +2,17 @@
 App({
  
   onLaunch: function () {
-    var that = this;
-    wx.login({
-      success: res => {
-        wx.request({
-          url: that.globalData.wx_url_1 + res.code + that.globalData.wx_url_2,
-          success: res => {
-            that.globalData.openid = res.data.openid;
-          }
-        })
-      }
-    });
+    // var that = this;
+    // wx.login({
+    //   success: res => {
+    //     wx.request({
+    //       url: that.globalData.wx_url_1 + res.code + that.globalData.wx_url_2,
+    //       success: res => {
+    //         that.globalData.openid = res.data.openid;
+    //       }
+    //     })
+    //   }
+    // });
   },
   globalData: {
     baseUrl: "http://192.168.0.66:2599",
